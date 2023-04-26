5131d5f21e8ad2a41f7c821ac747617dff7a2395
jwroblewski1 <131850302+jwroblewski1@users.noreply.github.com>
2023-04-26 08:39:54 +0200

    Update repoplik1.py

--- repoplik1.py
+++ repoplik1.py
@@ -1,4 +1,12 @@
 print("Jak masz na imię?")
 imie = input()
-
 print("Cześć " ,imie, " !")
+
+print("Czy chcesz podać swój wiek?")
+odpowiedz = input()
+
+if odpowiedz == tak:
+  input("Podaj imie:" )
+  
+elif odpowiedz == nie:
+  print("No trudno")
